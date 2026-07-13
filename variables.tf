5700a96183fdd56d75d77aaf61e965e7c90c7b85
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:39:06 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -24,8 +24,8 @@ EOT
     recurrence_period_start_date = string
     recurrence_type              = string
     subscription_id              = string
-    active                       = optional(bool)   # Default: true
-    file_format                  = optional(string) # Default: "Csv"
+    active                       = optional(bool)
+    file_format                  = optional(string)
     export_data_options = object({
       time_frame = string
       type       = string
